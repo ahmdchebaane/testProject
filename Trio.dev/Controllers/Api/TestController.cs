47f2e77d72e17ab8a517df126738c54de68cd161
ahmed chebaane <ahmed_prep@live.fr>
2018-05-25 14:46:15 +0200

    First Commit

---
+++ Trio.dev/Controllers/Api/TestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Trio.dev.Controllers.Api
+{
+    public class TestController : ApiController
+    {
+        public IHttpActionResult Gettest()
+        {
+           return Ok("Hello TRIO!");
+        }
+
+    }
+}
---
+++ Trio.dev/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Trio.dev.WebApiApplication" Language="C#" %>
