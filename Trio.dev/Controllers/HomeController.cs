760533ed68e4a77d57b6df78f8a0cbcab145ef8b
pritiben ambalal padhizar <pritip761@gmail.com>
2018-05-25 15:27:59 +0200

    second commit

--- Trio.dev/Controllers/HomeController.cs
+++ Trio.dev/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace Trio.dev.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            ViewBag.Title = "Home Page Priti";
 
             return View();
         }
